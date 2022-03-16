4ca525fdadbb6062791e7d147a9aa7226281c831
hanshaobo <hanshaobo@gw.com.cn>
2022-03-16 19:18:12 +0800

    init

---
+++ src/main/java/com/luka/ding/ding/DingApplication.java
@@ -0,0 +1,12 @@
+package com.luka.ding.ding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DingApplication.class, args);
+	}
+}
---
+++ src/test/java/com/luka/ding/ding/DingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luka.ding.ding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
